98146c80fa3804a62474091fa29b8ec10a8c6a4e
yassyolo <yoana.yotova03@gmail.com>
2024-10-20 01:43:29 +0300

    Added extension for finding the email through the user

---
+++ LogiTrack/Extensions/ClaimsPrincipalExtension.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Claims;
+
+namespace LogiTrack.Extensions
+{
+    public static class ClaimsPrincipalExtension
+    {
+        public static string GetUserId(this ClaimsPrincipal claimsPrincipal)
+        {
+            return claimsPrincipal.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+        public static string GetEmail(this ClaimsPrincipal claimsPrincipal)
+        {
+            return claimsPrincipal.FindFirstValue(ClaimTypes.Email);
+        }
+    }
+}
