1039f0815bf3139c76e2597765231108c88f8162
yassyolo <yoana.yotova03@gmail.com>
2024-10-20 01:41:07 +0300

    Added data access layer for communicating with the database

---
+++ LogiTrack.Infrastructure/Repository/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace LogiTrack.Infrastructure.Repository
+{
+    public interface IRepository
+    {
+        IQueryable<T?> All<T>() where T : class;
+        IQueryable<T?> AllReadonly<T>() where T : class;
+        Task<T?> GetById<T>(object id) where T : class;
+        Task AddAsync<T>(T entity) where T : class;
+        Task DeleteAsync<T>(T entity) where T : class;
+        Task<int> SaveChangesAsync();
+    }
+}
