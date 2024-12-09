e132ef8279473870aaf401f4cf2b770dd20be19c
yassyolo <yoana.yotova03@gmail.com>
2024-12-09 03:39:18 +0200

    Registered services

--- LogiTrack/Program.cs
+++ LogiTrack/Program.cs
@@ -7,32 +7,34 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllersWithViews();
+    builder.Services.AddScoped<IEmailSenderService, EmailSenderService>();
+    builder.Services.AddHttpClient<GeocodingService>();
+    builder.Services.AddScoped<IGoogleDriveService, GoogleDriveService>();
+    builder.Services.AddScoped<IRepository, Repository>();
+    builder.Services.AddScoped<IEventService, EventService>();
+    builder.Services.AddScoped<IDashboardService, DashboardService>();
+    builder.Services.AddScoped<IStatisticsService, StatisticsService>();
+    builder.Services.AddScoped<IDeliveryStatisticsService, DeliveryStatisticsService>();
+    builder.Services.AddScoped<IFuelPriceService, FuelPriceService>();
+    builder.Services.AddScoped<IUserService, UserService>();
+    builder.Services.AddScoped<IHomeService, HomeService>();
+    builder.Services.AddScoped<IClientsService, ClientsService>();
+    builder.Services.AddScoped<IVehicleService, VehicleService>();
+    builder.Services.AddScoped<IDeliveryService, DeliveryService>();
+    builder.Services.AddScoped<IDriverService, DriverService>();
+    builder.Services.AddScoped<ICashRegisterService, CashRegisterService>();
+    builder.Services.AddScoped<IRequestService, RequestService>();
+    builder.Services.AddScoped<IOfferService, OfferService>();
+    builder.Services.AddScoped<IInvoiceService, InvoiceService>();
+    builder.Services.AddHostedService<NotificationsService>();
+
+
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
-builder.Services.AddScoped<IEmailSenderService, EmailSenderService>();
-builder.Services.AddHttpClient<GeocodingService>();
-builder.Services.AddScoped<IGoogleDriveService, GoogleDriveService>();
-builder.Services.AddScoped<IRepository, Repository>();
-builder.Services.AddScoped<IEventService, EventService>();
-builder.Services.AddScoped<IDashboardService, DashboardService>();
-builder.Services.AddScoped<IStatisticsService, StatisticsService>();
-builder.Services.AddScoped<IDeliveryStatisticsService, DeliveryStatisticsService>();
-
-builder.Services.AddScoped<IFuelPriceService, FuelPriceService>();
-builder.Services.AddScoped<IUserService, UserService>();
-builder.Services.AddScoped<IHomeService, HomeService>();
-builder.Services.AddScoped<IClientsService, ClientsService>();
-builder.Services.AddScoped<IVehicleService, VehicleService>();
-builder.Services.AddScoped<IDeliveryService, DeliveryService>();
-builder.Services.AddScoped<IDriverService, DriverService>();
-builder.Services.AddScoped<ICashRegisterService, CashRegisterService>();
-builder.Services.AddScoped<IRequestService, RequestService>();
-builder.Services.AddScoped<IOfferService, OfferService>();
-builder.Services.AddScoped<IInvoiceService, InvoiceService>();
 //TODO: Add password constraints
 builder.Services.AddIdentity<IdentityUser, IdentityRole>(opt =>
 {
@@ -41,25 +43,45 @@ builder.Services.AddIdentity<IdentityUser, IdentityRole>(opt =>
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddDefaultTokenProviders();
 
+
+builder.Services.AddRazorPages();
+builder.Services.AddHttpClient();
+
+builder.Services.AddControllersWithViews();
+builder.Logging.AddConsole(options => options.LogToStandardErrorThreshold = LogLevel.Debug);
+
+
 var app = builder.Build();
 
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
- 
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error/500");
+    app.UseStatusCodePagesWithRedirects("/Home/Error?statusCode={0}");
     app.UseHsts();
 }
 
-
-app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
+app.UseHttpsRedirection();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-app.Run();
+try
+{
+    app.Run();
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error during startup: {ex.Message}");
+    throw;
+}
